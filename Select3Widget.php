e4230c37044b9d3c0fc301f4ca9d1f46cf91e30b
Cristian Salazar H <proyectos@chileshift.cl>
2017-10-06 21:19:58 -0300

    basic markup + css

--- Select3Widget.php
+++ Select3Widget.php
@@ -20,10 +20,32 @@ class Select3Widget extends Widget
 
     public $attribute;
 
+    public $prompt = "(Choose)";
+
     public function run()
     {
         $context = Yii::$app->controller;
 
         Select3Asset::register($context->view);
+   
+        $markup = $this->getMarkup();
+
+        return $markup;
     }
+
+    private function getMarkup()
+    {
+        return 
+        "
+            <div class='select3'>
+                <div class='inner'>
+                    <div class='select' >
+                        <div class='text'><span class='text-color'>{$this->prompt}</span></div>
+                        <div class='activator bg-color'><div class='caret-down caret-color'></div></div>
+                    </div>
+                </div>
+            </div> 
+        ";
+    }
+
 }

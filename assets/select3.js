a07457d27a76a3e55315d5f90872c0f6de0fab7e
Cristian Salazar H <proyectos@chileshift.cl>
2017-10-09 15:31:39 -0300

    'rebind' and 'load' functions

--- assets/select3.js
+++ assets/select3.js
@@ -14,12 +14,49 @@ jQuery(document).ready(function($){
         $('.select3').find('.options').hide();
     });
 
+    var select3bind = function(widget)
+    {
+        widget.find('[type=checkbox]').change(function(){
+           
+            var hidden = widget.find("input[type=hidden]");
+            
+            var clicked = $(this);
+            
+            var checkboxes = $(clicked.attr("data-group")).find("[type=checkbox]");
+
+            var allWasClicked = ('all' == clicked.attr('data-type'));
+            
+            var obj = {};
+
+            checkboxes.each(function(i, chk){
+            
+                var iAmAll = ('all' == $(chk).attr('data-type'));
+                
+                var isDisabled = $(chk).is(":disabled");
+
+                var key = $(chk).val();
+                
+                if(allWasClicked && !iAmAll && !isDisabled)
+                {
+                    $(chk).prop('checked', clicked.is(':checked'));
+                }
+
+                if(!iAmAll)
+                Object.defineProperty( obj , key , 
+                    { value: $(chk).is(':checked'), writable: 
+                        true, enumerable: true, configurable: true });
+
+            });
+
+            hidden.val(window.btoa(JSON.stringify(obj))); 
+        });
+    };
+
     $.fn.select3 = function()
     {
 		var _this = this;
 		$(_this).each(function(){
 			var widget = $(this);
-            var hidden = widget.find("input[type=hidden]");
             var options = widget.find('.options');
             
             var activator = function()
@@ -43,41 +80,46 @@ jQuery(document).ready(function($){
            
             options.css({ 'z-index' : 1000000 });
 
-            widget.find('[type=checkbox]').change(function(){
-                
-                var clicked = $(this);
-                
-                var checkboxes = $(clicked.attr("data-group")).find("[type=checkbox]");
-
-                var allWasClicked = ('all' == clicked.attr('data-type'));
-                
-                var obj = {};
-
-                checkboxes.each(function(i, chk){
-                
-                    var iAmAll = ('all' == $(chk).attr('data-type'));
-                    
-                    var isDisabled = $(chk).is(":disabled");
-
-                    var key = $(chk).val();
-                    
-                    if(allWasClicked && !iAmAll && !isDisabled)
-                    {
-                        $(chk).prop('checked', clicked.is(':checked'));
-                    }
-
-                    if(!iAmAll)
-                    Object.defineProperty( obj , key , 
-                        { value: $(chk).is(':checked'), writable: 
-                            true, enumerable: true, configurable: true });
-
-                });
-
-                hidden.val(window.btoa(JSON.stringify(obj))); 
-            });
-
+            select3bind(widget);
 		});
 	};
+    
+    $.fn.select3rebind = function(widget)
+    {
+        select3bind(widget);
+    };
+        
+    $.fn.select3load = function(widget, items, allLabel)
+    {
+        var options = widget.find('.options');
+               
+        options.find('.option').remove();
+                
+        var item = '<label class=\'option option-value option-border-color noselect\'>'
+            +'<input type=\'checkbox\' data-group=\''
+                +'#'+widget.attr('id')+'\' data-type=\'value\' value=\'\'>'
+            +'</label>';
+        
+        var counter = 0;
+        $.each(items, function(key,text){ counter++; });
+
+        if(counter > 1)
+        {
+            var option = options.append(item).find('.option:last-child');
+            option.removeClass('option-value');
+            option.addClass('option-all');
+            option.find('input').attr('data-type', 'all');
+            option.append(allLabel);
+        }
+
+        $.each(items, function(key,text){
+            var option = options.append(item).find('.option:last-child');
+            option.find('input').attr('value', key);
+            option.append(text);
+        });
+
+        $.fn.select3rebind(widget);
+    };
 
     $('.select3').select3();
 
